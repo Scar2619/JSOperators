ad5c9649b571e56e919d355029ba63f6f90f6da1
Katelind Phillips <scarlettkate94@gmail.com>
2022-07-07 17:52:02 -0400

    finished

---
+++ app.js
@@ -0,0 +1,43 @@
+//ex 1
+let a = 20;
+let b = 4;
+console.log("add:");
+console.log(a + b);
+console.log("sub:");
+console.log(a - b);
+console.log("mult:");
+console.log(a * b);
+console.log("div:");
+console.log(a / b);
+
+//ex 2
+let c = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+console.log(c + str);
+console.log(c + str2);
+console.log(c + isPresent);
+console.log(c + firstName);
+console.log(c + lastName);
+
+//ex 3
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+console.log(d == str3);
+console.log(d === str3);
+console.log(!isPresent2);
+console.log("eleven" == str4 && d >= str3);
+console.log(!isPresent2 || isPresent2);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
